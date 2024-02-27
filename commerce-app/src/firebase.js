07d8c7d06cca3c0a48b51b002510c620f83ec094
bhuvan-mg <bhuvanmg.211cs110@nitk.edu.in>
2024-02-27 08:35:14 +0530

    start of home page

---
+++ commerce-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAf0UHuzdWEzquGTBAo-5dP5ujYaBHeYgk",
+    authDomain: "commerce-app-4e011.firebaseapp.com",
+    projectId: "commerce-app-4e011",
+    storageBucket: "commerce-app-4e011.appspot.com",
+    messagingSenderId: "3488107734",
+    appId: "1:3488107734:web:033f9ed9f7195953ab0d77",
+    measurementId: "G-V90G8LNQ44"
+  };
